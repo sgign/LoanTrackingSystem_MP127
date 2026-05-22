2b1612100d15238e01df5261cba02805dd2f257f
Ani Isabelle L. Sibal <alsibal@up.edu.ph>
2026-05-23 00:07:27 +0800

    payment features added

---
+++ backend/src/main/java/com/loantracker/backend/controller/PaymentController.java
@@ -0,0 +1,33 @@
+package com.loantracker.backend.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.loantracker.backend.entity.Payment;
+import com.loantracker.backend.repository.PaymentRepository;
+
+@RestController
+@RequestMapping("/api/payments")
+@CrossOrigin(origins = "*") 
+public class PaymentController {
+
+    @Autowired
+    private PaymentRepository paymentRepository;
+
+    @GetMapping
+    public List<Payment> getAllPayments() {
+        return paymentRepository.findAll();
+    }
+
+    @PostMapping
+    public Payment createPayment(@RequestBody Payment payment) {
+        return paymentRepository.save(payment);
+    }
+}
